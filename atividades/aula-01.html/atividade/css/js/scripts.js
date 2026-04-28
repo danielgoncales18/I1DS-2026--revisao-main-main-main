e034a006f6edcd85f92340530f32b065bfc7d814
danielgoncales18 <danielgoncalesferreira16@gmail.com>
2026-04-28 16:55:38 -0300

    atividade html

---
+++ atividades/aula-01.html/atividade/css/js/scripts.js
@@ -0,0 +1,35 @@
+let usercout
+
+
+
+const cadastrar = (event) => {
+  // Parar a propagação padrão do evento
+  event.preventDefault();
+  // Capturar os valores do elementos por Id
+  let nome = document.getElementById("nome").value;
+  let email = document.getElementById("email").value;
+  // Capturando o elemento de lista por Id
+  let lista = document.getElementById("lista");
+  
+  usercout++;
+  let id = usercout
+ 
+ 
+  // Criar um novo elemento <li>Nome - Email</li>
+  let novoItem = document.createElement("li");
+  novoItem.innerText = id + " - " + email;
+  // Adicionar o novo item na lista ja existente
+  lista.appendChild(novoItem);
+
+
+
+  // Limpar os campos
+  document.getElementById("nome").value = "";
+  document.getElementById("email").value = "";
+
+
+}
+    
+ 
+
+}
